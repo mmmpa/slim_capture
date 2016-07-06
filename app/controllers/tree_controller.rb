5ae763dd8a829a94e8626648f870c96bc6325f09
mmmpa <mmmpa.mmmpa@gmail.com>
2016-07-07 04:52:44 +0900

    worked

---
+++ app/controllers/tree_controller.rb
@@ -0,0 +1,36 @@
+class TreeController < ApplicationController
+  def index
+@tree = [
+  {
+    title: :a,
+    children: [
+      {
+        title: :a_a
+      },
+      {
+        title: :a_c,
+        children: [
+          {
+            title: :a_c_a
+          },
+          {
+            title: :a_c_b,
+            children: [
+              {
+                title: :a_c_b_a
+              }
+            ]
+          },
+          {
+            title: :a_c_c
+          }
+        ]
+      },
+      {
+        title: :a_a
+      }
+    ]
+  }
+]
+  end
+end
